9fa10f4ca39fca5913bf82951f9d91871a85e1a8
jmon83 <76069447+jmon83@users.noreply.github.com>
2021-11-24 11:06:57 -0500

    mathGame
    Learning NodeJS for high score

--- index.node.js
+++ index.node.js
@@ -1,12 +1,29 @@
+/*const express = require('express');
+const app = express();
 const fs = require('fs');
-console.log('Node here');
+let maybeThisTime;
 
-fs.readFile('./score.JSON', 'utf8', (err, data) =>{
+fs.readFile('./score.JSON', 'utf8', (err, data) => {
   if (err) {
-    console.log(err);
+    console.log(`Error reading file from disk: ${err}`);
   } else {
-    const hiScore = JSON.parse(data);
+    maybeThisTime = JSON.parse(data);
+    console.log(maybeThisTime.id);
+  } });
 
-  }
-})
+app.get('/getScore/', function(req, res) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
+  res.end(maybeThisTime);
+});
 
+app.post('/setScore/', function(req, res) {
+    let aNewHigh = req;
+    let aTryString = JSON.stringify(aNewHigh);
+    fs.writeFileSync('score.JSON', aTryString);
+});
+
+app.listen(63342);
+
+console.log('Server started.');
+*/
--- js/main.js
+++ js/main.js
@@ -2,41 +2,124 @@ let theCount = 0;
 let yourScore = 0;
 let helloDialog = document.getElementById('helloDialog');
 let theProblem = document.getElementById('theNumbers');
-let tickUp;
+let aQuestion;
+let highScoreHolder;
+let highScore1 = document.getElementById('highScore');
 let progBarMeter = document.getElementById('progBarMeter');
+let thisScore = document.getElementById('thisScore');
+let yourScoreDisp = document.getElementById('yourScore');
 
+//Timer
 function aTick(){
   let interval = setInterval(function () {
     theCount++;
     console.log(theCount);
-    updateTheMeter(theCount);
-    if (theCount == 15) {
+    thisScore.textContent = ` ${yourScore}`;
+    updateTheMeter();
+    if (theCount === 15) {
       clearInterval(interval);
       endGame();
     }
   }, 1100);
 }
 
-let updateTheMeter = function (theCount) {
-  progBarMeter.style.width = `"${theCount}0%"`;
+let updateTheMeter = function () {
+  document.querySelector('#progBarMeter');
+  switch (theCount) {
+    case 4:
+      $("#progBarMeter").addClass("bg-success");
+      progBarMeter.style.width = '10%';
+      break;
+    case 5:
+      $("#progBarMeter").addClass("bg-success");
+      progBarMeter.style.width = '15%';
+      break;
+    case 6:
+      $("#progBarMeter").addClass("bg-success");
+      progBarMeter.style.width = '20%';
+      break;
+    case 7:
+      $("#progBarMeter").addClass("bg-success");
+      progBarMeter.style.width = '30%';
+      break;
+    case 8:
+      $("#progBarMeter").addClass("bg-warning");
+      progBarMeter.style.width = '40%';
+      break;
+    case 9:
+      $("#progBarMeter").addClass("bg-warning");
+      progBarMeter.style.width = '50%';
+      break;
+    case 10:
+      $("#progBarMeter").addClass("bg-warning");
+      progBarMeter.style.width = '60%';
+      break;
+    case 11:
+      $("#progBarMeter").addClass("bg-warning");
+      progBarMeter.style.width = '70%';
+      break;
+    case 12:
+      $("#progBarMeter").addClass("bg-danger");
+      progBarMeter.style.width = '80%';
+      break;
+    case 13:
+      $("#progBarMeter").addClass("bg-danger");
+      progBarMeter.style.width = '90%';
+      break;
+    case 14:
+      $("#progBarMeter").addClass("bg-danger");
+      progBarMeter.style.width = '95%';
+      break;
+    case 15:
+      $("#progBarMeter").addClass("bg-danger");
+      progBarMeter.style.width = '100%';
+      break;
+
+  }
 }
 
 function playGame(){
+  theCount = 0;
+  yourScore = 0;
   countDown();
   gameStart();
 }
 
 let gameStart = function(){
+  displayProblem();
   aTick();
 }
 
+let endGame = function() {
+  progBarMeter.style.width = '0%';
+  yourScoreDisp.textContent = ` ${yourScore}`;
+  $('#byeDialog').modal('toggle');
+ /* if (yourScore > highScoreHolder){ NODEJS!
+    setHighScore(yourScore);
+    highScore1 = yourScore;
+  }*/
+};
 
-//Get or Set High Score
-let highScore = function(){
-  };
-
-//Timer
-
+/*Get or Set High Score - still learning Nodejs
+let setHighScore = function(score){
+  $.ajax({
+    type: 'POST',
+    url: '/getScore',
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    data: JSON.stringify({
+      score: {
+        id: score
+      }
+    }),
+    success: function (response) {
+     console.log("success!");
+    },
+    error: function (xhr, status, err) {
+      console.log(xhr.responseText);
+    }
+  });
+}*/
 
 //Countdown
 let countDown = function() {
@@ -57,12 +140,13 @@ $('#c3').fadeIn(500, function(){
 });
 };
 
+//Make the math problems
 let getNumbers = function(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max-min + 1) + min);
 }
-//Make the math problems
+
 let makeProblem = function () {
   let num1 = getNumbers(1, 10);
   let num2 = getNumbers(1, 10);
@@ -90,11 +174,66 @@ let makeProblem = function () {
   return theQuestion;
 }
 
+let displayProblem = function(){
+  $('#theNumbers').fadeOut(300);
+  aQuestion = makeProblem();
+  theProblem.textContent = aQuestion.question;
+  $('#theNumbers').fadeIn(325);
+}
+
 //take the answer
+let answerTheQuestion = function(input, answer){
+  if (input === answer) {
+    yourScore++;
+    theCount--
+    //Green when correct
+    $("#theNumbers").addClass("flash");
+    setTimeout( function(){
+      $("#theNumbers").removeClass("flash");
+    }, 300);
+    $('#answerField').val('');
+    displayProblem();
+  } else {
+    //Red when wrong
+    $("#theNumbers").addClass("wrongFlash");
+    setTimeout( function(){
+      $("#theNumbers").removeClass("wrongFlash");
+    }, 300);
+    $('#answerField').val('');
+    displayProblem();
+  }
+};
 
-//increment or decrement timer
-
-
-$(document).ready(function (){
+$(document).ready(function () {
   $('#helloDialog').modal('toggle');
+
+  $(function() {
+    $('#answerField').keypress(function (e) {
+      if ((e.which && e.which === 13) || (e.keyCode && e.keyCode === 13)) {
+        $('button[type=submit] .default').click();
+        e.preventDefault();
+        answerTheQuestion(Number($(this).val()), aQuestion.answer);
+        return false;
+      } else {
+        return true;
+      }
+    });
+  });
+
+
+//Still learning Nodejs
+  $.ajax({
+    type: 'GET',
+    url: '../score.JSON',
+    withCredentials: true,
+    success: function (response) {
+      let myHiScore = JSON.parse(response);
+      highScoreHolder = myHiScore.id;
+      highScore1.textContent = myHiScore.id;
+      console.log(myHiScore);
+    },
+    error: function (xhr, status, err) {
+      console.log(xhr.responseText);
+    }
+  });
 });
